6304689e1d841387c16388d888ba7004547ad5f4
Cassidy Williams <cassidoo@outlook.com>
2013-10-19 13:32:45 -0500

    Reorganized Zach's code

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,12 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key is used for verifying the integrity of signed cookies.
+# Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
-
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-# You can use `rake secret` to generate a secure secret key.
-
-# Make sure your secret_key_base is kept private
-# if you're sharing your code publicly.
-Splash::Application.config.secret_key_base = 'f734eceebb1df8604780117a29fac06a2f97106a5c4f48731f142ab6539438fac05c091f8ac3b04eaad91cc0dca8b4cb47242b92ba27932f789622c885e3dcac'
+Splash::Application.config.secret_token = '13fb6b839e31e3cbcee614a0834ad37360ed30a70da8e7859a51853690c9a304dddf48b9cba8b91be43823d8a261916999283180899f7e5848ee581edc1fd6b3'
