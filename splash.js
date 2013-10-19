1b8afaef543037b8950501bcaeb82c0f65ea1d64
Zach Plata <zplata@iastate.edu>
2013-10-19 15:50:51 -0500

    final commit

--- splash.js
+++ splash.js
@@ -3,24 +3,9 @@
  */
 
 <script src="//cdnjs.cloudflare.com/ajax/libs/annyang/0.2.0/annyang.min.js"></script>
-<script>
-if (annyang) {
-  // Let's define our first command. First the text we expect, and then the function it should call
-  var commands = {
-    'show tps report': function() {
-      $('#tpsreport').animate({bottom: '-100px'});
-    }
-  };
-
-  // Initialize annyang with our commands
-  annyang.init(commands);
-
-  // Start listening. You can call this here, or attach this call to an event, button, etc.
-  annyang.start();
-}
-</script>
-
 <script type = "text/javascript">
+
+	var totalPoints = 0;
 	//Var for a subject to learn
 	var subject = prompt("Choose a subject");
 	var deck = [];
@@ -28,8 +13,8 @@ if (annyang) {
 	// Explains card and puts in front and back side data
 	var card = [];
 	var implementCard = function(){
-		var frontData = prompt("What goes on this side?");
-		var backData = prompt("What goes on this side?");
+		var frontData = man;
+		var backData = woman;
 		card = [frontData, backData];
 		return card;	
 	};
@@ -44,21 +29,46 @@ if (annyang) {
 	*/
 	
 	//Add cards to said deck
-	var keepGoing = prompt("Want to add cards to deck? Y or N?");
-	while(keepGoing === "Y"){
+	var keepGoing = true;
+	while(keepGoing === true){
 		deck.push(implementCard());
-		keepGoing = prompt("Keep going? Y or N?");
+		keepGoing = false;
 	}
 	
 	//Var for what side of card user sees
-	var chooseSideToLookAt = prompt("Choose a side: 'front','back', or 'random'");
 	
 	//For front side cards
-	if(chooseSide === "front"){
+
 		for(var i = 0; i < deck.length; i++){
 			var temp = deck[i];
-			
+			if (annyang) {
+  			// Let's define our first command. First the text we expect, and then the function it should call
+  				var commands = {
+    				'*term': compare,
+    				'please abort the mission': exit 
+  				};
+  
+  				var compare = function(term) {
+    				if(term === temp[1]){
+    					points = points + 1;
+    				}	
+    				else{
+    					totalPoints = totalPoints - 1;
+    				}
+  				}
+  			
+  				var exit = function(please abort the mission){
+  					break;
+  				}
+
+  			// Initialize annyang with our commands
+ 			annyang.init(commands);
+	
+  			// Start listening. You can call this here, or attach this call to an event, button, etc.
+  			annyang.start();
+			}
 		}
-	}
+		console.log("You have: " + totalPoints);
+	
 	
 </script>
